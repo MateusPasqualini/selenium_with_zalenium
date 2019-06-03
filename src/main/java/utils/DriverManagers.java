8fa8b2ded96e43f12fdc36e308e72191f5c078a9
MateusPasqualini <pasquamat@gmail.com>
2019-06-03 16:18:10 -0300

    managers

--- src/main/java/utils/DriverManagers.java
+++ src/main/java/utils/DriverManagers.java
@@ -10,16 +10,19 @@ import static org.openqa.selenium.remote.BrowserType.FIREFOX;
 
 public class DriverManagers {
 
+    private DriverManagers(){
+        throw new IllegalStateException("DriverManagers class cannot be instantiated");
+    }
 
     public static WebDriver setDriver(String driverType) {
         WebDriver driverManager = null;
 
         if (driverType.equals(CHROME)) {
             WebDriverManager.chromedriver().setup();
-            driverManager = new ChromeDriver();
+            driverManager = new ChromeDriver(OptionsManager.getChromeOptions());
         } else if (driverType.equals(FIREFOX)) {
             WebDriverManager.firefoxdriver().setup();
-            driverManager = new FirefoxDriver();
+            driverManager = new FirefoxDriver(OptionsManager.getFirefoxOptions());
         }
         return driverManager;
     }
---
+++ src/main/java/utils/OptionsManager.java
@@ -0,0 +1,40 @@
+package utils;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxDriverLogLevel;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
+
+public class OptionsManager {
+
+    private OptionsManager() {
+        throw new IllegalStateException("OptionsManager class");
+    }
+
+    public static ChromeOptions getChromeOptions() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--start-maximized");
+        chromeOptions.addArguments("--ignore-certificate-errors");
+        chromeOptions.addArguments("--disable-popup-blocking");
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("--log-level=3");
+        chromeOptions.addArguments("--silent");
+        return chromeOptions;
+    }
+
+    public static FirefoxOptions getFirefoxOptions() {
+        FirefoxOptions geckoOptions = new FirefoxOptions();
+        FirefoxProfile profile = new FirefoxProfile();
+        geckoOptions.addArguments("--headless");
+        geckoOptions.setCapability("marionette", true);
+        geckoOptions.setLogLevel(FirefoxDriverLogLevel.FATAL);
+        profile.setAcceptUntrustedCertificates(true);
+        profile.setAssumeUntrustedCertificateIssuer(false);
+        profile.setPreference("network.proxy.type", 0);
+        geckoOptions.setCapability(FirefoxDriver.PROFILE, profile);
+        return geckoOptions;
+    }
+
+}
