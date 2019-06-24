cc3e4ff58d4106b1cd3b98b8eee6c1af0b2b9346
MateusPasqualini <pasquamat@gmail.com>
2019-06-24 13:40:43 -0300

    :whale: Adding thread-safe parallel testing with zalenium and adding docker-compose for multi-container deployment

--- src/main/java/utils/DriverManagers.java
+++ src/main/java/utils/DriverManagers.java
@@ -2,40 +2,47 @@ package utils;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-
-import static org.openqa.selenium.remote.BrowserType.CHROME;
-import static org.openqa.selenium.remote.BrowserType.FIREFOX;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DriverManagers {
 
+
+    private static final Logger logger = Logger.getLogger(DriverManagers.class.getName());
+
+
     private DriverManagers() {
-        throw new IllegalStateException("DriverManagers class cannot be instantiated");
     }
 
-    public static WebDriver setDriver(String driverType) {
-        RemoteWebDriver driverManager = null;
+    private static ThreadLocal<RemoteWebDriver> tlDriver = new ThreadLocal<>();
 
-        if (driverType.equals(CHROME)) {
-            WebDriverManager.chromedriver().setup();
+    public static synchronized void setTLDriver(String browser) {
+        if (browser.equals("firefox")) {
             try {
-                driverManager = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), OptionsManager.getChromeOptions());
+                WebDriverManager.firefoxdriver().setup();
+                tlDriver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), OptionsManager.getFirefoxOptions()));
             } catch (MalformedURLException e) {
-                e.printStackTrace();
+                logger.log(Level.SEVERE, "Endereço especificado não encontrado");
             }
-        } else if (driverType.equals(FIREFOX)) {
-            WebDriverManager.firefoxdriver().setup();
+        } else if (browser.equals("chrome")) {
             try {
-                driverManager = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), OptionsManager.getFirefoxOptions());
+                WebDriverManager.chromedriver().setup();
+                tlDriver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), OptionsManager.getChromeOptions()));
             } catch (MalformedURLException e) {
-                e.printStackTrace();
+                logger.log(Level.SEVERE, "Endereço especificado não encontrado");
+            }
             }
         }
-        return driverManager;
+
+    public static synchronized WebDriver getTLDriver() {
+        return tlDriver.get();
+    }
+
+    public static synchronized void shutdownThread() {
+        tlDriver.remove();
     }
 }
--- src/test/java/com/automation/tests/TestBase.java
+++ src/test/java/com/automation/tests/TestBase.java
@@ -1,22 +1,27 @@
 package com.automation.tests;
 
+import com.automation.pages.HomePage;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.*;
 import utils.DriverManagers;
 
+import static utils.DriverManagers.getTLDriver;
+
 public class TestBase {
 
     protected WebDriver driver;
-
+    protected HomePage homePage;
     @BeforeMethod
     @Parameters(value = {"browser"})
-    public void setup(@Optional String browser) {
-        driver = DriverManagers.setDriver(browser);
+    public synchronized void setup(@Optional String browser) {
+        DriverManagers.setTLDriver(browser);
+        homePage = new HomePage(getTLDriver());
+
     }
 
     @AfterMethod
-    public void teardown() {
-        driver.quit();
+    public synchronized void teardown() {
+        getTLDriver().quit();
     }
 
     public WebDriver getDriver() {
